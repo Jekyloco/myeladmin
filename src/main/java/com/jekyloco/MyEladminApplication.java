99fb1ecf95328edb83388cfee44e2884bf77a315
Jekyloco <2581600267@qq.com>
2022-07-19 17:08:06 +0800

    2. 获取验证码

--- src/main/java/com/jekyloco/MyEladminApplication.java
+++ src/main/java/com/jekyloco/MyEladminApplication.java
@@ -8,6 +8,7 @@ public class MyEladminApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MyEladminApplication.class, args);
+        System.out.println("http://localhost:8080");
     }
 
 }
