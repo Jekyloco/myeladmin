5ca645787d750ec107ea452a55f9bb5f2540010a
Jekyloco <2581600267@qq.com>
2022-07-19 09:54:47 +0800

    1. 初始化项目

---
+++ src/test/java/com/jekyloco/MyEladminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jekyloco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyEladminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
